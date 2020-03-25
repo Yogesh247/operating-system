eaf46464ff8495978fceb74f89516e08dc41b128
Yogesh247 <53824309+Yogesh247@users.noreply.github.com>
2020-03-25 21:58:17 +0530

    Create problem1.c

---
+++ problem1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+int x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11;
+//for declearing diffierent cylinder number
+
+int dis1,dis2,dis3,dis4,dis5,dis6,dis7,dis8,dis9,dis10,dis11;
+//distance between two cylinders
+
+//valuesof cylinder
+
+x1=125;
+
+
+x2=143;
+
+x3=86;
+
+x4=1470;
+
+x5=913;
+
+x6=1774;
+
+x7=948;
+
+x8=1509;
+
+x9=1022;
+
+x10=1750;
+
+x11=130;
+
+printf("previos request at cylinder number is %d \n",x1);
+
+printf("currently serving a request at cylinder number is %d \n",x2);
+
+dis1=x1-x2;
